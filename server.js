b4376d566028c5fff90b2c35258a76bde0e09e00
Aaron Ealdama <aaronealdama@gmail.com>
2020-05-05 22:33:33 -0500

    heroku stuff

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const PORT = 3000;
 
@@ -15,9 +16,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
 // routes
