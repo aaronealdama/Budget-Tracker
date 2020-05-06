5314c18dc0b8f35da68724c9f8e022d85f49f47d
Aaron Ealdama <aaronealdama@gmail.com>
2020-05-05 22:05:01 -0500

    fixed this application

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,48 +1,55 @@
 const FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/styles.css",
-    "/index.js",
-    "/db.js",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png"
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/index.js",
+  "/db.js",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
 ];
 
-const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
+const PRECACHE = "precache-v1";
+const RUNTIME = "runtime";
 
-self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(PRECACHE)
-        .then(cache => cache.addAll(FILES_TO_CACHE))
-        .then(self.skipWaiting())
-    );
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(PRECACHE)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then(self.skipWaiting())
+  );
 });
 
-self.addEventListener("activate", event => {
-    const currentCaches = [PRECACHE, RUNTIME];
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return cacheNames.filter(cacheName => !currentCaches.includes(cacheName))
-        }).then(cachesToDelete => {
-            return Promise.all(cachesToDelete.map(cacheToDelete => {
-                return caches.delete(cacheToDelete);
-            }));
-        }).then(() => self.clients.claim())
-    );
-});
-
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-      caches.match(e.request).then((r) => {
-            console.log('[Service Worker] Fetching resource: '+e.request.url);
-        return r || fetch(e.request).then((response) => {
-                  return caches.open(RUNTIME).then((cache) => {
-            console.log('[Service Worker] Caching new resource: '+e.request.url);
-            cache.put(e.request, response.clone());
-            return response;
-          });
-        });
+self.addEventListener("activate", (event) => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return cacheNames.filter(
+          (cacheName) => !currentCaches.includes(cacheName)
+        );
       })
-    );
-  });
+      .then((cachesToDelete) => {
+        return Promise.all(
+          cachesToDelete.map((cacheToDelete) => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.open(RUNTIME).then((cache) => {
+      console.log("[Service Worker] Fetching resource: " + e.request.url);
+      return fetch(e.request).then((response) => {
+        console.log(response);
+        cache.put(e.request.url, response.clone());
+        return response;
+      });
+    })
+  );
+});
